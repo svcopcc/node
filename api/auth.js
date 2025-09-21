d1c8a5f48ea51e321c33b6bb94769b4f9cbe1069
svcopcc <svcopcc@gmail.com>
2025-09-22 00:12:18 +0800

    簡化OAuth授權流程

--- api/auth.js
+++ api/auth.js
@@ -1,23 +1,20 @@
-const { google } = require('googleapis');
-
 export default async function handler(req, res) {
     try {
-        const oauth2Client = new google.auth.OAuth2(
-            process.env.GOOGLE_CLIENT_ID,
-            process.env.GOOGLE_CLIENT_SECRET,
-            `https://${req.headers.host}/api/auth/callback`
-        );
-
+        const clientId = process.env.GOOGLE_CLIENT_ID;
+        const redirectUri = `https://${req.headers.host}/api/auth/callback`;
+        
         const scopes = [
             'https://www.googleapis.com/auth/drive.file',
             'https://www.googleapis.com/auth/spreadsheets'
-        ];
+        ].join(' ');
         
-        const url = oauth2Client.generateAuthUrl({
-            access_type: 'offline',
-            scope: scopes,
-            prompt: 'consent'
-        });
+        const url = `https://accounts.google.com/o/oauth2/v2/auth?` +
+            `client_id=${clientId}&` +
+            `redirect_uri=${encodeURIComponent(redirectUri)}&` +
+            `scope=${encodeURIComponent(scopes)}&` +
+            `response_type=code&` +
+            `access_type=offline&` +
+            `prompt=consent`;
         
         res.redirect(url);
     } catch (error) {
